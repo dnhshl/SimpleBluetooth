19c56f3d13683cd1e83c93fca6cc538eb25ff5bc
ba1999 <andrej.boldt@outllok>
2021-03-23 15:47:30 +0100

    Zugriff auf die Liste der bereits gekoppelten Geräte

--- app/src/main/java/com/example/simplebluetooth/MainActivity.kt
+++ app/src/main/java/com/example/simplebluetooth/MainActivity.kt
@@ -4,10 +4,16 @@ import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 import splitties.toast.toast
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val buttonPairedDevices : Button by lazy{ findViewById(R.id.buttonPairedDevices) }
+    private val listview : ListView by lazy{ findViewById(R.id.listview) }
 
     private lateinit var mBluetooth : BluetoothAdapter
 
@@ -21,6 +27,10 @@ class MainActivity : AppCompatActivity() {
             toast(getString(R.string.bt_not_available))
             finish()
         }
+
+        buttonPairedDevices.setOnClickListener {
+            getPairedDevices()
+        }
     }
 
     override fun onResume() {
@@ -29,5 +39,24 @@ class MainActivity : AppCompatActivity() {
             val turnBTOn = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(turnBTOn, 1)
         }
+    }
+
+    private fun getPairedDevices() {
+
+        val pairedDevices = mBluetooth.bondedDevices
+        val list = ArrayList<Any>()
+
+        if (pairedDevices.size > 0) {
+            for (bt in pairedDevices) {
+                list.add("""${bt.name}${bt.address}""".trimIndent())
+            }
+        } else {
+            toast(getString(R.string.bt_no_paired_devices))
+        }
+
+        val adapter: ArrayAdapter<*> = ArrayAdapter(this,
+                R.layout.support_simple_spinner_dropdown_item,
+                list)
+        listview.adapter = adapter
     }
 }
